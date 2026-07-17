2e519cae0ebcb97335feb6faaee09b713c24e0ca
Alejandro <mpl200340@gmail.com>
2026-07-16 20:00:51 -0500

    cambio landing

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -5,16 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    outDir: '../public',
-    emptyOutDir: false,
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      }
-    }
-  }
 })
